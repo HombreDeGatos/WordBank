1ae814abf90ea2a03c4ac9d89c45338479a0327f
Jared Katzman <jaredleekatzman@gmail.com>
2015-05-11 19:39:13 -0400

    Changed to searchbar UI and added wordList

---
+++ WordBank/AppearanceBridger.h
@@ -0,0 +1,13 @@
+//
+//  AppearanceBridger.h
+//  WordBank
+//
+//  Created by Jared Katzman on 5/11/15.
+//  Copyright (c) 2015 deGatos. All rights reserved.
+//
+
+#ifndef WordBank_AppearanceBridger_h
+#define WordBank_AppearanceBridger_h
+
+
+#endif
---
+++ WordBank/AppearanceBridger.m
@@ -0,0 +1,9 @@
+//
+//  AppearanceBridger.m
+//  WordBank
+//
+//  Created by Jared Katzman on 5/11/15.
+//  Copyright (c) 2015 deGatos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
